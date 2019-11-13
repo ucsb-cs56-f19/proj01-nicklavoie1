c18fe1558919f2c1f72e9b16f2ae90b80807c08a
NickLavoie1 <35705872+NickLavoie1@users.noreply.github.com>
2019-11-13 14:49:04 -0800

    fixed2

--- src/main/java/hello/GithubOrgMembershipService.java
+++ src/main/java/hello/GithubOrgMembershipService.java
@@ -57,6 +57,7 @@ public class GithubOrgMembershipService implements MembershipService {
     /**
      check role
      * @param oAuth2AuthenticationToken oauth token 
+     * @param String role
      * @return is current logged in user a member or an admin of the github org?
      * */
 
