9794c1662c50c4e885a8ba276dc81ca1c0cabcf0
Nick Lavoie <ntl@ucsb.edu>
2019-11-20 21:17:00 -0800

    nl - create Form

---
+++ src/main/java/hello/EqSearch.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class EqSearch {
+    
+    private int distance;
+    private int minmag;
+
+    public void setDistance(int distance) { this.distance = distance; }
+
+    public void setMinmag(int minmag) { this.minmag = minmag; }
+
+    public int getDistance() { return distance; }
+
+    public int getMinmag() { return minmag; }
+}
